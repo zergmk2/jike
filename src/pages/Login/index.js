dff94c701d170cc2eea19f3382307915657d8fa9
Lu_Cao <zergmk2@gmail.com>
2023-10-26 00:00:18 +0800

    add login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,29 @@
+import "./index.scss";
+import { Card, Form, Input, Button } from "antd";
+import logo from "@/assests/logo.svg";
+
 const Login = () => {
-  return <div> this is a Login</div>;
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* login form */}
+        <Form>
+          <Form.Item>
+            <Input size="large" placeholder="phone number"></Input>
+          </Form.Item>
+          <Form.Item>
+            <Input size="large" placeholder="capcha"></Input>
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large">
+              submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
 };
 
 export default Login;
